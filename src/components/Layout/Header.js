c191c30663f9b825809b9dc21a5c0d22576045b3
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-04-04 18:24:06 +0200

    work on Header component, add style and background pic

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
-import mealsImage from "../../assets/meals.jpg";
+import mealsImage from "../../assets/meals (1).jpg";
+import classes from "../Layout/Header.Module.css";
 
 const Header = (props) => {
   return (
     <React.Fragment>
-      <header>
+      <header className={classes.header}>
         <h1>React Meals</h1>
         <button>Cart</button>
       </header>
-      <div> {mealsImage} </div>
+      <div>
+        <img src={mealsImage} alt="mealsImage" />
+      </div>
     </React.Fragment>
   );
 };
